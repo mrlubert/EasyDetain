6af0e3de574dbf8a7b86e05cdd2c3fdaf47eff95
Nicholas DeNicola <mr.lubert@aol.com>
2022-05-10 11:41:12 -0400

    Update
    Switched item from totem to saddle
    added baby / adult villagers
    added villagers exp
    added villagers level
    added health
    cant rename item
    can right click on block and place the mob there or right click the air to place on you

--- src/main/java/plugin/vertex101/easydetain/events/entityEvent.java
+++ src/main/java/plugin/vertex101/easydetain/events/entityEvent.java
@@ -1,48 +1,63 @@
 package plugin.vertex101.easydetain.events;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.*;
-import org.bukkit.entity.Villager.Profession;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import plugin.vertex101.easydetain.EasyDetain;
 
+@SuppressWarnings("unused")
 public class entityEvent implements Listener {
 
-    @EventHandler
-    public void onCapture(PlayerInteractEntityEvent e) {
-        Entity entity = e.getRightClicked();
-        Player player = e.getPlayer();
+	@EventHandler
+	public void onCapture(PlayerInteractEntityEvent e) {
+		Entity entity = e.getRightClicked();
+		Player player = e.getPlayer();
 
-        if(player.isSneaking()) {
-            if(entity instanceof LivingEntity) {
-                NamespacedKey type = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_type");
-                NamespacedKey rez = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_rez");
-                NamespacedKey prof = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_prof");
-                ItemStack itemStack = new ItemStack(Material.TOTEM_OF_UNDYING);
-                ItemMeta itemMeta = itemStack.getItemMeta();
+		if (player.isSneaking()) {
+			if (entity instanceof LivingEntity) {
+				NamespacedKey type = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_type");
+				NamespacedKey health = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_health");
+				NamespacedKey rez = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_rez");
+				NamespacedKey prof = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_prof");
+				NamespacedKey exp = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_exp");
+				NamespacedKey level = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_level");
+				NamespacedKey age = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_age");
+				ItemStack itemStack = new ItemStack(Material.SADDLE);
+				ItemStack air = new ItemStack(Material.AIR);
+				ItemMeta itemMeta = itemStack.getItemMeta();
+				PersistentDataContainer getData = itemMeta.getPersistentDataContainer();
+				if (entity.getType() == EntityType.VILLAGER) {
+					Villager villager = (Villager) entity;
+					itemMeta.setDisplayName(entity.getName());
+					getData.set(health, PersistentDataType.DOUBLE, villager.getHealth());
+					getData.set(type, PersistentDataType.STRING, entity.getType().toString());
+					getData.set(age, PersistentDataType.INTEGER, villager.getAge());
+					getData.set(prof, PersistentDataType.STRING, villager.getProfession().toString());
+					getData.set(rez, PersistentDataType.STRING, "rez");
+					getData.set(exp, PersistentDataType.INTEGER, villager.getVillagerExperience());
+					getData.set(level, PersistentDataType.INTEGER, villager.getVillagerLevel());
 
-                if(entity.getType() == EntityType.VILLAGER) {
-                    Villager villager = (Villager) entity;
-                    itemMeta.setDisplayName(entity.getName());
-                            itemMeta.getPersistentDataContainer().set(type, PersistentDataType.STRING, entity.getType().toString());
-                    itemMeta.getPersistentDataContainer().set(prof, PersistentDataType.STRING, villager.getProfession().toString());
-                    itemMeta.getPersistentDataContainer().set(rez, PersistentDataType.STRING, "rez");
-                } else {
-                    itemMeta.setDisplayName(entity.getName());
-                    itemMeta.getPersistentDataContainer().set(type, PersistentDataType.STRING, entity.getType().toString());
-                    itemMeta.getPersistentDataContainer().set(rez, PersistentDataType.STRING, "rez");
-                }
+				} else {
+					itemMeta.setDisplayName(entity.getName());
+					getData.set(type, PersistentDataType.STRING, entity.getType().toString());
+					getData.set(health, PersistentDataType.DOUBLE, ((LivingEntity) entity).getHealth());
+					getData.set(rez, PersistentDataType.STRING, "rez");
+				}
 
-                itemStack.setItemMeta(itemMeta);
-                player.getInventory().addItem(itemStack);
-                entity.remove();
-            }
-        }
-    }
+				itemStack.setItemMeta(itemMeta);
+				player.getInventory().addItem(itemStack);
+				entity.remove();
+			}
+		}
+	}
 }
--- src/main/java/plugin/vertex101/easydetain/events/playerEvent.java
+++ src/main/java/plugin/vertex101/easydetain/events/playerEvent.java
@@ -3,13 +3,16 @@ package plugin.vertex101.easydetain.events;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.Villager.Profession;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -18,31 +21,90 @@ import plugin.vertex101.easydetain.EasyDetain;
 
 public class playerEvent implements Listener {
 
-    @EventHandler
-    public void onRelease(PlayerInteractEvent e) {
-        Player player = e.getPlayer();
-        Action action = e.getAction();
-        NamespacedKey type = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_type");
-        NamespacedKey prof = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_prof");
-        ItemStack itemStack = player.getInventory().getItemInMainHand();
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        PersistentDataContainer container = itemMeta.getPersistentDataContainer();
+	@EventHandler
+	public void onRename(InventoryClickEvent e) {
 
-        if(action.equals(Action.RIGHT_CLICK_AIR) && player.getInventory().getItemInMainHand().getType().equals(Material.TOTEM_OF_UNDYING)) {
-            if(container.has(type, PersistentDataType.STRING)) {
-                String getType = container.get(type, PersistentDataType.STRING);
-                if(getType.equalsIgnoreCase("villager")) {
-                    String getProf = container.get(prof, PersistentDataType.STRING);
+		if (e.getInventory() instanceof AnvilInventory)
+			if (e.getInventory().contains(Material.SADDLE)) {
+				NamespacedKey rez = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_rez");
+				ItemStack itemStack = e.getCurrentItem();
+				ItemMeta itemMeta = itemStack.getItemMeta();
+				PersistentDataContainer container = itemMeta.getPersistentDataContainer();
+				if (container.has(rez, PersistentDataType.STRING)) {
+					if (e.getClickedInventory() == e.getInventory() && container.has(rez, PersistentDataType.STRING)
+							&& e.getSlot() == 1)
+						e.setCancelled(true);
+				}
+				if (e.getClickedInventory() == e.getInventory() && container.has(rez, PersistentDataType.STRING)
+						&& e.getSlot() == 2)
+					e.setCancelled(true);
+			}
 
-                    Villager villager = (Villager) player.getWorld().spawnEntity(player.getLocation(), EntityType.VILLAGER);
-                    villager.setProfession(Profession.valueOf(getProf));
+	}
 
-                    player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-                } else {
+	@EventHandler
+	public void onRelease(PlayerInteractEvent e) {
+		Player player = e.getPlayer();
+		Action action = e.getAction();
+		NamespacedKey type = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_type");
+		NamespacedKey health = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_health");
+		NamespacedKey prof = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_prof");
+		NamespacedKey exp = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_exp");
+		NamespacedKey level = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_level");
+		NamespacedKey age = new NamespacedKey(EasyDetain.getInstance(), "easy_detain_age");
+		ItemStack itemStack = player.getInventory().getItemInMainHand();
+		ItemMeta itemMeta = itemStack.getItemMeta();
 
-                }
+		if (itemMeta != null) {
+			PersistentDataContainer container = itemMeta.getPersistentDataContainer();
 
-            }
-        }
-    }
+			if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)
+					&& player.getInventory().getItemInMainHand().getType().equals(Material.SADDLE)) {
+				if (container.has(type, PersistentDataType.STRING)) {
+					String getType = container.get(type, PersistentDataType.STRING);
+					Double gethealth = container.get(health, PersistentDataType.DOUBLE);
+					if (getType.equalsIgnoreCase("villager")) {
+						String getProf = container.get(prof, PersistentDataType.STRING);
+						Integer getlevel = container.get(level, PersistentDataType.INTEGER);
+						Integer getage = container.get(age, PersistentDataType.INTEGER);
+						int getexp = container.get(exp, PersistentDataType.INTEGER);
+
+						if (action.equals(Action.RIGHT_CLICK_AIR)) {
+							Villager villager = (Villager) player.getWorld().spawnEntity(player.getLocation(),
+									EntityType.VILLAGER);
+							villager.setVillagerExperience(getexp);
+							villager.setVillagerLevel(getlevel);
+							villager.setAge(getage);
+							villager.setHealth(gethealth);
+							villager.setProfession(Profession.valueOf(getProf));
+						}
+						if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
+							Villager villager = (Villager) player.getWorld()
+									.spawnEntity(e.getClickedBlock().getLocation().add(0, 1, 0), EntityType.VILLAGER);
+							villager.setVillagerExperience(getexp);
+							villager.setVillagerLevel(getlevel);
+							villager.setAge(getage);
+							villager.setHealth(gethealth);
+							villager.setProfession(Profession.valueOf(getProf));
+						}
+					}
+					if (!getType.equalsIgnoreCase("villager") && action.equals(Action.RIGHT_CLICK_AIR)) {
+						EntityType mobtype = EntityType.valueOf(getType);
+						LivingEntity mob = (LivingEntity) player.getWorld().spawnEntity(player.getLocation(), mobtype);
+						mob.setHealth(gethealth);
+					}
+					if (!getType.equalsIgnoreCase("villager") && action.equals(Action.RIGHT_CLICK_BLOCK)) {
+						EntityType mobtype = EntityType.valueOf(getType);
+						LivingEntity mob = (LivingEntity) player.getWorld()
+								.spawnEntity(e.getClickedBlock().getLocation().add(0, 1, 0), mobtype);
+						mob.setHealth(gethealth);
+					}
+					player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+				} else {
+
+				}
+
+			}
+		}
+	}
 }
